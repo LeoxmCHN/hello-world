87912e6a40b761e1ee5d0428cc19801783b161bb
xueming.liu <xueming.liu@ubtrobot.com>
2018-05-04 17:07:07 +0800

    初始化

---
+++ GITTEST/GITTEST/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GITTEST
+//
+//  Created by ubt on 2018/5/4.
+//  Copyright © 2018年 Xueming Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
