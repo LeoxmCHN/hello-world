1f59980daa3caddfa2824b42afa47409878cfd8b
xueming.liu <xueming.liu@ubtrobot.com>
2018-05-04 17:01:59 +0800

    第一次提交

---
+++ GitTEST/GitTEST/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTEST
+//
+//  Created by ubt on 2018/5/4.
+//  Copyright © 2018年 Xueming Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitTEST/GitTESTTests/GitTESTTests.m
@@ -0,0 +1,39 @@
+//
+//  GitTESTTests.m
+//  GitTESTTests
+//
+//  Created by ubt on 2018/5/4.
+//  Copyright © 2018年 Xueming Liu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GitTESTTests : XCTestCase
+
+@end
+
+@implementation GitTESTTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
